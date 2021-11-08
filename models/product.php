14cf4befd9f9118d6886255d2aea1fbfde9c7748
Loi <lytatloizz.no1@gmail.com>
2021-11-09 06:50:50 +0700

    first commit

---
+++ models/product.php
@@ -0,0 +1,40 @@
+<?php
+class Product extends Db
+{
+    public function getAllProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getTenProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM `products` WHERE id LIMIT 10");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function Search($keyword)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `name` LIKE ?");
+        $keyword = "%$keyword%";
+        $sql->bind_param("s", $keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getProductById($id)
+    {
+        $sql = self::$connection->prepare("SELECT* FROM product WHERE id = ?");
+        $sql->bind_param("i",$id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    
+}
